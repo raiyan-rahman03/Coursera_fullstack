7da32e7baf84e28f2cb431555cb006cb1d2a21c6
Raiyan-Rahman-03 <rakiyasultana0172@gmail.com>
2024-03-06 15:44:18 +0600

    sql con

--- FULLSTACK/myproject/myapp/models.py
+++ FULLSTACK/myproject/myapp/models.py
@@ -4,8 +4,9 @@ from django.db import models
 # Create your models here.
 class Booking(models.Model):
     first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length =200)
     reservation_date = models.DateField()
-    reservation_slot = models.SmallIntegerField(default=10)
+    reservation_slot = models.SmallIntegerField(default=10 )
 
     def __str__(self): 
         return self.first_name
--- FULLSTACK/myproject/myproject/settings.py
+++ FULLSTACK/myproject/myproject/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'reservations',
+        'USER': 'root',
+        'PASSWORD': 'root123',
+        'HOST': 'localhost',    # Or the IP address of your MySQL server
+        'PORT': '3306',         # Default MySQL port
     }
 }
 
